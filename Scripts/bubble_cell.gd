e3453982e5cd90463c743f1ebcdde9d6c623be17
David Crespo <david.crespo@correounivalle.edu.co>
2025-01-25 03:43:29 -0500

    Work in progress

--- Scripts/bubble_cell.gd
+++ Scripts/bubble_cell.gd
@@ -4,31 +4,45 @@ var is_active = false
 var level = 1
 
 var self_rigidbody
+var self_sprite
+var collision_shape
 
 func _ready():
 	self_rigidbody = $RigidBody2D
+	self_sprite = $RigidBody2D/Sprite2D
+	collision_shape = $RigidBody2D/Area2D/CollisionShape2D
 	is_active = true
+
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func rescale(size) -> void:
+	self_sprite.scale *= size
 	
+	if collision_shape.shape is CircleShape2D:
+		collision_shape.shape.radius = collision_shape.shape.radius * size  # Scale the radius
+
 func change_level(temp):
 	level = temp
-	$AnimationPlayer.play("spawn")
+	#$AnimationPlayer.play("spawn")
 	#scale = level * Vector2(1, 1) 
-	self.scale *= level
-		
-	print(self.scale)
+	self.position += Vector2(0, -100)
 	
 func change_is_active():
 	is_active = !is_active
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 
-	if body != self_rigidbody && body.is_in_group("bubble"):
-		if body.get_parent().has_method("change_is_active"):
+	if body != self_rigidbody && body.is_in_group("bubble") && body.get_groups() == self.get_groups():
+		if body.get_parent().has_method("change_is_active") && is_active:
 			body.get_parent().change_is_active()
-		if is_active:
-			body.queue_free()
-			change_level(level + 1)
+			body.get_parent().queue_free()
+			level += 1
+			rescale(level)
+			
+
+			
--- test.gd
+++ test.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 # Declare the variable to store the prefab scene
-var prefab_scene : PackedScene
+var blue_cell : PackedScene
+var red_cell : PackedScene
+var green_cell : PackedScene
+var yellow_cell : PackedScene
 
 func _ready():
 	# Load the prefab scene
-	prefab_scene = load("res://bubbleCell.tscn")
+	blue_cell = load("res://Bubbles/blueCell.tscn")
+	red_cell = load("res://Bubbles/redCell.tscn")
+	green_cell = load("res://Bubbles/greenCell.tscn")
+	yellow_cell = load("res://Bubbles/yellowCell.tscn")
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -15,6 +21,21 @@ func _input(event):
 
 func spawn_object(position: Vector2):
 	# Instance the prefab and position it at the clicked position
-	var new_object = prefab_scene.instantiate()
-	new_object.position = position  # Set its position
-	add_child(new_object)  # Add the new object to the scene
+	# Generate a random number between 1 and 4
+	var random_value = randi() % 4 + 1
+
+	# Instantiate the correct scene based on the random value
+	var cell_instance = null
+
+	match random_value:
+		1:
+			cell_instance = blue_cell.instantiate()
+		2:
+			cell_instance = red_cell.instantiate()
+		3:
+			cell_instance = green_cell.instantiate()
+		4:
+			cell_instance = yellow_cell.instantiate()
+
+	cell_instance.position = position  # Set its position
+	add_child(cell_instance)  # Add the new object to the scene
