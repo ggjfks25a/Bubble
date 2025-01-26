2a1fb5fc72e60778e7369840dbc974816c743ab3
David Crespo <david.crespo@correounivalle.edu.co>
2025-01-26 01:18:44 -0500

    Pan

--- Scripts/brekeable_area.gd
+++ Scripts/brekeable_area.gd
@@ -1,12 +1,47 @@
 extends Area2D
 
+@onready var timer1 = Timer.new()
+@onready var timer2 = Timer.new()
+
 var parent_node
 var is_active = false
+
+var is_broken = false
 
 func _ready() -> void:
 	parent_node = get_parent().get_parent().get_parent()
 	parent_node.connect("activate_breaker_mode", Callable(self, "_activate_breaker_mode"))
-	
+
+func _process(delta: float) -> void:
+	if is_broken:
+		is_broken = false
+		
+		# Load texture
+		var texture = load("res://Sprites/Bubble_Effect.png")
+		
+		# Set up timers
+		timer1.one_shot = true
+		timer2.one_shot = true
+		add_child(timer1)
+		add_child(timer2)
+		
+		# Start timers
+		timer1.start(0.025)
+		timer2.start(0.15)
+		
+		# Perform actions with timers
+		var sprite = get_parent().get_node("Sprite2D")
+		sprite.scale *= 1.2
+		await timer1.timeout  # Wait for timer1 to finish
+		sprite.texture = texture
+		sprite.scale *= 0.8
+		await timer2.timeout  # Wait for timer2 to finish
+		get_parent().get_parent().queue_free()
+
+
+func set_is_broken(val):
+	is_broken = val
+
 func calculate_adjacent_bubbles(parent_area, cummulated_areas):
 	var overlapping_areas = parent_area.get_overlapping_areas() 
 	for area in overlapping_areas:
@@ -20,9 +55,8 @@ func _input_event(viewport, event, shape_idx):
 		if event.is_action_pressed("click"):
 			var areas = calculate_adjacent_bubbles(self, [])
 			for area in areas:
-				area.get_parent().get_parent().queue_free()
-			await get_tree().physics_frame
-			get_parent().get_parent().queue_free()
+				area.set_is_broken(true)
+			is_broken = true
 
 func _activate_breaker_mode(val):
 	is_active = val
